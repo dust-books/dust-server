929568d9d93fcfedacc3e0063be4690093f0529a
Brad Cypert <brad.cypert@gmail.com>
2024-11-27 19:50:09 -0500

    start on session management-wip

--- src/users/data.ts
+++ src/users/data.ts
@@ -1,5 +1,8 @@
 import type { Database } from "../../database.ts";
-import type { User } from "./user.ts";
+import type { User, UserWithId } from "./user.ts";
+
+class NoValidTokenError extends Error {}
+class ExpiredTokenError extends Error {}
 
 export const migrate = (database: Database) => {
     return database.migrate([
@@ -55,15 +58,38 @@ export const addUser = (database: Database, user: User) => {
     });
 }
 
-export const createSession = (database: Database, user: User) => {
+export const createSession = (database: Database, user: UserWithId, token: string) => {
+    const expires = new Date();
+    // expires after 24 hours
+    expires.setDate(expires.getDate() + 1);
     return database.execute({
         sql: `
-        INSERT INTO sessions (display_name, email, password) VALUES ($display, $email, $password) 
+        INSERT INTO sessions (session_token, expires_at, user_id) VALUES ($token, $expires_at, $user) 
     `, args: {
-            display: user.displayName,
-            email: user.email,
-            // This _SHOULD_ be encrypted by this point
-            password: user.password
+            token,
+            expires_at: expires.toISOString(),
+            user: user.id
         }
     });
+}
+
+export const getUserIdFromSession = async (database: Database, token: string): Promise<number> => {
+    const result = await database.execute({
+        sql: `
+        SELECT * FROM sessions WHERE session_token = $token
+    `, args: {
+            token,
+        }
+    });
+
+    if (result.rows.length <= 0) {
+        throw new NoValidTokenError();
+    }
+
+    const expiresAt = new Date(result.rows[0]['expires_at'] as string);
+    if (expiresAt.getMilliseconds() <= new Date().getMilliseconds()) {
+        throw new ExpiredTokenError();
+    }
+
+    return result.rows[0]['user_id'] as number;
 }
--- src/users/user.ts
+++ src/users/user.ts
@@ -3,3 +3,5 @@ export interface User {
     email: string,
     password: string,
 }
+
+export type UserWithId = User & { id: number }
