e830464b918628ca94b2073c03200a9b9a65cd27
Brad Cypert <brad.cypert@gmail.com>
2024-11-28 18:22:13 -0500

    format

--- src/books/book-crawler.test.ts
+++ src/books/book-crawler.test.ts
@@ -5,26 +5,32 @@ import type { FileSystemWalker } from "./fs/fs-walker.ts";
 import type { WalkEntry } from "jsr:@std/fs/walk";
 
 class MockFSWalker implements FileSystemWalker {
-
-    collectOutput: Array<WalkEntry> = [];
+  collectOutput: Array<WalkEntry> = [];
   collect(): Promise<Array<WalkEntry>> {
     return Promise.resolve(this.collectOutput);
   }
 }
 
 describe("BookCrawler", () => {
-    describe("crawlForBooks", () => {
-        it("parses appropriate data from FSWalker", async () => {
-            const items = [{path: "M:/books/Henry James/The Portrait of a Lady/portrait.epub", isFile: true, isDirectory: false, isSymlink: false, name: "portrait.epub"}];
-            const mockFs = new MockFSWalker();
-            mockFs.collectOutput = items;
+  describe("crawlForBooks", () => {
+    it("parses appropriate data from FSWalker", async () => {
+      const items = [
+        {
+          path: "M:/books/Henry James/The Portrait of a Lady/portrait.epub",
+          isFile: true,
+          isDirectory: false,
+          isSymlink: false,
+          name: "portrait.epub",
+        },
+      ];
+      const mockFs = new MockFSWalker();
+      mockFs.collectOutput = items;
 
-            const crawler = new BookCrawler(mockFs);
-            const books = await crawler.crawlForBooks();
-            expect(books[0].author).toBe("Henry James")
-            expect(books[0].name).toBe("The Portrait of a Lady");
-            expect(books[0].filepath).toBe(items[0].path)
-        });
+      const crawler = new BookCrawler(mockFs);
+      const books = await crawler.crawlForBooks();
+      expect(books[0].author).toBe("Henry James");
+      expect(books[0].name).toBe("The Portrait of a Lady");
+      expect(books[0].filepath).toBe(items[0].path);
     });
   });
-  
+});
