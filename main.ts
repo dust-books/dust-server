c08b4aeb4e3bb950b5b03925df4bb8c137f55ff8
Brad Cypert <brad.cypert@gmail.com>
2024-12-25 21:50:26 -0500

    start on book streaming, not fully working

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "@oak/oak";
+import { Application, isHttpError, Router } from "@oak/oak";
 import type { Module } from "./module.ts";
 import { DustDatabase } from "./database.ts";
 import { UsersModule } from "./src/users/module.ts";
@@ -29,6 +29,28 @@ class DustService implements Service {
     this.config.collect();
     Deno.addSignalListener('SIGINT', this._abort);
     signal.addEventListener("abort", this.stop);
+    
+    // TODO: Let's pull this out. Maybe to a "core" module?
+    this.app.use(async (context, next) => {
+      try {
+        await next();
+      } catch (err) {
+        if (isHttpError(err)) {
+          context.response.status = err.status;
+          const { message, status, stack } = err;
+          if (context.request.accepts("json")) {
+            context.response.body = { message, status, stack };
+            context.response.type = "json";
+          } else {
+            context.response.body = `${status} ${message}\n\n${stack ?? ""}`;
+            context.response.type = "text/plain";
+          }
+        } else {
+          console.log(err);
+          throw err;
+        }
+      }
+    });
     // TODO: We keepin' this?
     this.router.get("/", (ctx) => {
       ctx.response.status = 200;
--- src/books/routes.ts
+++ src/books/routes.ts
@@ -1,28 +1,23 @@
-import { Status, type Router } from "@oak/oak";
+import { RouterContext, Status, type Router } from "@oak/oak";
 import { bookService } from "./book-service.ts";
 import { dustService } from "../../main.ts";
 
 export const registerRoutes = (router: Router) => {
-  router.get("/books/", async (ctx) => {
+  router.get("/books/", async (ctx: RouterContext<"/books/">) => {
     const books = await bookService.getBooks(dustService.database);
-    console.log(books);
     ctx.response.body = {
       books: books,
     };
   });
-  router.get("/books/:id", async (ctx) => {
+  router.get("/books/:id", async (ctx: RouterContext<"/books/:id">) => {
     try {
       const book = await bookService.getBookById(
         dustService.database,
         ctx.params.id
       );
-      console.log(book);
-      ctx.response.status = Status.OK;
       ctx.response.body = {
         book: book,
       };
-      ctx.response.type = "json";
-
       return;
     } catch (e) {
       console.error(e);
@@ -34,7 +29,8 @@ export const registerRoutes = (router: Router) => {
       ctx.params.id
     );
     const file = await Deno.open(book.filepath, { read: true });
-    ctx.response.body = file;
+    ctx.response.body = file.readable;
+    file.close();
   });
   router.get("/books/:id/progress", (ctx) => {
     ctx.response.body = {
